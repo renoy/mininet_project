5b21e568483412938cb7dd5d3fcea79df2f801f5
renoy <renoy.johnmarattukalam@sjsu.edu>
2013-11-13 15:08:38 -0800

    First commit of files

---
+++ in_band_test.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+"""
+-Build a network of desired depth and fanout.
+-In-band controller.
+-All the hosts start sending traffic to a particular host.
+-Traffic consists of certain # of data packets of a specified packet size and then a packet to generate a control traffic.
+-Latency for the new packet to reach the destination host after the flow table being populated to the respective OVSs.
+- % Packet Loss
+- Iperf between the host and the controller
+"""
+
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.node import Node, OVSSwitch, Controller, RemoteController
+from mininet.link import Link
+from mininet.log import setLogLevel, info
+from mininet.topolib import TreeNet, TreeTopo
+
+class InbandController( RemoteController ):
+
+    def checkListening( self ):
+        "Overridden to do nothing."
+        return
+
+def topo_init():
+	myTopo = TreeTopo( depth=3, fanout=3 )
+	net = Mininet( topo=myTopo, switch=OVSSwitch, build=False )
+
+	"Creating Controller"
+	c0 = net.addController( 'c0', controller=InbandController, ip='10.0.0.100' )
+	net.start()
+
+	server = net.addHost( 'server', ip='10.0.0.100')
+	#net.addLink( h0, s1 )
+	
+	"Starting controller on Switch s1 alone"	
+	#c0.start()
+	#net.build()
+	s1 = net.get('s1')
+	link = net.addLink( server, s1 )
+	s1.attach( link.intf2 )
+	net.configHosts()
+	#s1.cmd( 'ifconfig s1 10.0.0.50' )
+	#s1.start( [ c0 ] )
+	#hosts_list = net.hosts
+	#no_of_hosts = len( hosts_list )
+	#print "# of hosts: %d" %no_of_hosts
+	#net.start()
+        s1 = net.get('s1')
+        s1.cmd( 'ifconfig s1 10.0.0.50' )
+	s2 = net.get('s2')
+	s2.cmd( 'ifconfig s2 10.0.0.51' )
+	s3 = net.get('s3')
+        s3.cmd( 'ifconfig s3 10.0.0.52' )
+	s4 = net.get('s4')
+        s4.cmd( 'ifconfig s4 10.0.0.53' )
+	s5 = net.get('s5')
+        s5.cmd( 'ifconfig s5 10.0.0.54' )
+	s6 = net.get('s6')
+        s6.cmd( 'ifconfig s6 10.0.0.55' )
+	s7 = net.get('s7')
+        s7.cmd( 'ifconfig s7 10.0.0.56' )
+	s8 = net.get('s8')
+        s8.cmd( 'ifconfig s8 10.0.0.57' )
+	s9 = net.get('s9')
+        s9.cmd( 'ifconfig s9 10.0.0.58' )
+	s10 = net.get('s10')
+        s10.cmd( 'ifconfig s10 10.0.0.59' )
+	s11 = net.get('s11')
+        s11.cmd( 'ifconfig s11 10.0.0.60' )
+	s12 = net.get('s12')
+        s12.cmd( 'ifconfig s12 10.0.0.61' )
+	s13 = net.get('s13')
+        s13.cmd( 'ifconfig s13 10.0.0.62' )
+	#server_host = hosts_list[-1]
+	#info( server_host.cmd( 'pwd' ) )
+	#server_host.cmd( 'xterm h27' )
+	#server_host.cmd( '(./listener.sh)&' )
+	#print "Line after listener script"
+	#info( server_host.cmd( 'ifconfig' ) )
+	#del hosts_list [0]
+	#net.start()
+	#for n in hosts_list:
+		#n.cmd( 'sudo wireshark &' )
+		#n.cmd( './sender.sh' )
+	CLI( net )
+	net.stop()
+
+if __name__ == '__main__':
+	setLogLevel('info')
+	topo_init()
+	#traffic_gen()
+
