f045a932a2cfa433b290e92a383ef4a97c54ad01
santhosh2488 <santhosh2488@gmail.com>
2013-11-27 23:38:35 -0800

    Update sender.sh
    Changes to ping during mid of ncat send

--- sender.sh
+++ sender.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #sender.sh
-#three args needed as: count, server-host ip, new ip
- 
+#three args needed as: count, server-host ip, ping_ip. The script starts picking up a random sleep time.
+
+
+sleep=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+
+n=`expr $RANDOM % 21`
+sleep  ${sleep[n]}
 count=0
- 
+mid=`expr $1 / 2`
+echo $mid
 while [ $count -lt $1 ]
- 
+
 do
-         
+
         count=`expr $count + 1`
-    perl -e 'print "A"x1' | nc $2 2222
+        if [ "$count" -eq "$mid" ] ;
+        then
+                (ping $3  -c 1 >> ping.txt 2>&1)&
+        fi
+        perl -e 'print "A"x1' | nc $2 2222
 done
-#rm ping.txt
-#touch ping.txt
-(ping $3  -c 1 >> ping.txt 2>&1)& 
