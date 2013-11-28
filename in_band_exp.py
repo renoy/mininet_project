b75c96e8f74b39d2a287a777566c8a91febcb12a
Renoy John <renoy.johnmarattukalam@sjsu.edu>
2013-11-27 23:50:24 -0800

    Update in_band_exp.py
    Updated with changes to tshark command and deleted unwanted lines.

--- in_band_exp.py
+++ in_band_exp.py
@@ -52,74 +52,44 @@ def topo_init():
     "TODO: Automate assigning of IP addresses"
     ip = hosts_list[-2].IP()
  
-        "Assigning IP addresses to switches in the network"
-        switch_list = net.switches
-        n = len(switch_list)
-        ip = hosts_list[-2].IP()
+    "Assigning IP addresses to switches in the network"
+    switch_list = net.switches
+    n = len(switch_list)
+    ip = hosts_list[-2].IP()
  
-        for i in range(1,n+1):
+    for i in range(1,n+1):
+            ip2int = lambda ipstr: struct.unpack( '!I', socket.inet_aton(ipstr))[0]
+            int_ip = ip2int(ip)
+            int_ip+=1
+            int2ip = lambda n: socket.inet_ntoa(struct.pack('!I', n))
+            ip = int2ip(int_ip)
  
-                ip2int = lambda ipstr: struct.unpack( '!I', socket.inet_aton(ipstr))[0]
-                int_ip = ip2int(ip)
-                int_ip+=1
-                int2ip = lambda n: socket.inet_ntoa(struct.pack('!I', n))
-                ip = int2ip(int_ip)
- 
-                s = net.get('s%d' % i)
-                s.cmd( 'ifconfig s%d %s ' % (i, ip) )
+            s = net.get('s%d' % i)
+            s.cmd( 'ifconfig s%d %s ' % (i, ip) )
  
     "Start the controller in 'server' host"
     global controller_host 
     controller_host = hosts_list[-1]
-    #info( controller_host.cmd( 'controller -v ptcp:6633 &' ) )
- 
+
     "Fetch the listener host in the network"
     global server_host 
     server_host = hosts_list[-2]
-    #server_host.cmd( 'pwd' )
- 
-    "Start the listener script in the listener host"
-    #info( server_host.cmd( './listener.sh &' ) )
-    #server_host.cmd( 'while true; do nc -l -p 2222; done > /home/mininet/mininet/custom/dump.txt &' )
- 
-    "Start the sender script in the remaining hosts"
-    #del hosts_list [0]
-    #num_sender_hosts = num_hosts-2
-    #print num_sender_hosts
-    #for n in hosts_list[:num_sender_hosts]:
-        #server_host.cmd( 'while true; do nc -l -p 2222; done > /home/mininet/mininet/custom/dump.txt &' )
-        #info( n.cmd( 'ifconfig | grep inet' ) )
-        #n.cmd( './sender.sh &' )
- 
-    #CLI( net )
-    #net.stop()
  
 def test_run():
-    num_sender_hosts = num_hosts-2
-    #print num_sender_hosts
-    #print controller_host
+    num_sender_hosts = num_hosts-9
     controller_host.cmd( 'cd ~' )
-    controller_host.cmd( 'sudo tshark  >>tshark.txt 2>&1  &' )
+    controller_host.cmd( 'sudo tshark -w /var/tmp/tshark64_1K 2>&1 &' )
     server_ip = server_host.IP()
     ping_ip = hosts_list[-3].IP()
     controller_host.cmd( 'controller -v ptcp:6633 >>controller.txt 2>&1 &' ) 
-    #net.pingAll()
-    #print server_host
     server_host.cmd( 'ncat -l 2222 --keep-open >>dump.txt 2>&1 &' )
-    #server_host.cmd( 'cat dump.txt | ncat -l 2222 --keep-open > /home/mininet/mininet/custom/out.txt 2>&1 &' )
-        for n in hosts_list[:num_sender_hosts]:
-                #n.cmd( 'ifconfig | grep inet' )
-                n.popen( 'sudo bash sender.sh 100 %s %s > sender.txt 2>&1 &' % (server_ip, ping_ip) )
-                #server_hosl.cmd( 'while true; do nc -l -p 2222; done > /home/mininet/mininet/custom/dump.txt &' )
-     
-    #net.pingAll()
- 
+    for n in hosts_list[:num_sender_hosts]:
+            n.popen( 'sudo bash sender.sh 100 %s %s > sender.txt 2>&1 &' % (server_ip, ping_ip) )
     CLI( net )
  
     controller_host.cmd( 'pkill controller' )
     server_host.cmd( 'pkill ncat' )
     server_host.cmd( 'sudo rm ping.txt dump.txt' )          
-    controller_host.cmd( 'sudo rm ~/tshark.txt' )
     net.stop()
  
 if __name__ == '__main__':
